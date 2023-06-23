96668961bbac3868aea52222fc6e690581a4eabd
geekysilento <adityakarna.ak@gmail.com>
2023-06-23 18:02:30 +0545

    did fileserver questions

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -59,8 +59,8 @@ app.delete('/todos/:id', (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
 
 module.exports = app;
